d83deae01ad01b947f7c5966ed49bef4d6f7c3b4
k <brood915@gmail.com>
2017-06-16 11:49:24 -0400

    added collapsing effect with pure angularjs

--- src/app/common/nav/nav.component.js
+++ src/app/common/nav/nav.component.js
@@ -1,6 +1,16 @@
 import template from './nav.html';
 
+class NavCtrl {
+  constructor() {
+  }
+  $onInit() {
+    this.isCollapsed = true;
+  }
+}
+
+
 export const NavComponent = {
+  controller: NavCtrl,
   template,
   bindings: {}
 }
